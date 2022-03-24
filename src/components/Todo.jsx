d47b4acde4ced863e381e04bc7d13ed6a0882969
SalihTpc <salihtopcu@hotmail.com.tr>
2022-03-25 02:48:48 +0300

    Todo.jsx changed

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,30 +1,45 @@
 import React from "react";
+import axios from "axios";
 
 const Todo = ({ text, todo, todos, setTodos }) => {
   const deleteHandler = () => {
-    setTodos(todos.filter((el) => el.id !== todo.id));
+    // setTodos(todos.filter((el) => el.id !== todo.id));
+    async function deleteTodo() {
+      await axios.delete(
+        `https://todo-django-restapi.herokuapp.com/api/task-delete/${todo.id}`
+      );
+    }
+    deleteTodo();
   };
   const completeHandler = () => {
-    setTodos(
-      todos.map((item) => {
-        if (item.id === todo.id) {
-          console.log(item.id);
-          return {
-            ...item,
-            completed: !item.completed,
-          };
-        }
-        return item;
-      })
-    );
+    const updatedTodo = { title: todo.title, completed: !todo.completed };
+    async function changeTodo() {
+      await axios.post(
+        `https://todo-django-restapi.herokuapp.com/api/task-update/${todo.id}`,
+        updatedTodo
+      );
+    }
+    changeTodo();
+    // setTodos(
+    //   todos.map((item) => {
+    //     if (item.id === todo.id) {
+    //       console.log(item.id);
+    //       return {
+    //         ...item,
+    //         completed: !item.completed,
+    //       };
+    //     }
+    //     return item;
+    //   })
+    // );
   };
   return (
     <div className={`todo ${todo.completed ? "completed" : ""}`}>
-      <button onClick={completeHandler} className="complete-btn">
+      <button type="button" onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <li className="todo-item">{text}</li>
-      <button onClick={deleteHandler} className="trash-btn">
+      <button type="button" onClick={deleteHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
